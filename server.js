c287ff0565ee8b93d06c793f7a10e1f72233021f
joujou.park@gmail.com <Jabran>
2024-08-18 20:22:42 +0100

    Initial commit

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
@@ -12,8 +11,6 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
 const PORT = process.env.PORT || 3500;
-
-
 
 const app = express();
 
@@ -28,22 +25,55 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
-// Serve static files from public directory
-app.use('/', express.static(path.join(__dirname, 'public')));
+// Serve static files from the public and views/css directories
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'views', 'css')));
+app.use('/img', express.static(path.join(__dirname, 'views', 'img')));
+app.use('/js', express.static(path.join(__dirname, 'views', 'js')));
+app.use('/scss', express.static(path.join(__dirname, 'views', 'scss')));
+app.use('/fonts', express.static(path.join(__dirname, 'views', 'fonts')));
 
-// Serve the login page from the views directory
+
+// Routes to serve additional HTML pages
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'login.html'));
 });
+app.get('/dashboard', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'dashboard.html'));
+});
+app.get('/tables', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'tables.html'));
+});
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+app.get('/profile', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'profile.html'));
+});
+app.get('/rtl', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'rtl.html'));
+});
+app.get('/sign-in', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'sign-in.html'));
+});
+app.get('/sign-up', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'sign-up.html'));
+});
+app.get('/virtual-reality', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'virtual-reality.html'));
+});
+app.get('/billing', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'billing.html'));
+});
 
-// Define API routes
+// API routes
 app.use('/', require('./routes/root'));
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
 app.use('/refresh', require('./routes/refresh'));
 app.use('/logout', require('./routes/logout'));
 
-// Apply JWT verification middleware
+// Apply JWT verification middleware for protected routes
 app.use(verifyJWT);
 app.use('/user', require('./routes/api/user'));
 app.use('/client', require('./routes/api/client'));
